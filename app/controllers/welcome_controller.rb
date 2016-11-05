de41fee7bd17b18f6c4798f62006ca95b27e14a1
wendelhuang <weiwei5987@126.com>
2016-11-05 08:50:19 +0800

    layout

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,25 @@
 class WelcomeController < ApplicationController
   def index
   end
+  def login
+    puts params[:password]
+    if params[:username] == 'hww' and params[:password] == '666666'
+      redirect_to action: 'main'
+    else
+      redirect_to action: 'index'
+    end
+  end
+  def main
+    @electric_unit = 0.55
+    @electric_charge = []
+    @electric_charge << {name: '张三', remain: 7.8}
+    @electric_charge << {name: '李四', remain: 17.5}
+    @electric_charge << {name: '王五', remain: 23.3}
+
+    @water_unit = 1.2
+    @water_charge = []
+    @water_charge << {name: '张三', remain: 7.8}
+    @water_charge << {name: '李四', remain: 17.5}
+    @water_charge << {name: '王五', remain: 23.3}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  get 'welcome/main'
+  post 'welcome/login'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'welcome#index'
